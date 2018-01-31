81c29d7dd251cc7f449239dd635ad1c3febd3133
josepaez2 <josepaez_2@hotmail.com>
2018-01-31 17:55:07 -0500

    Generates calories controller & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+	
+  root 'calories#index'
+  resources :calories#, except: [:edit, :update, :show, :destroy]
+ 	
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
