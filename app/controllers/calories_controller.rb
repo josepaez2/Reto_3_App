87038dec08ba9b9f131ba18551e77af9db3f37b6
josepaez2 <josepaez_2@hotmail.com>
2018-02-01 00:17:28 -0500

    Sets params in controllers

--- app/controllers/calories_controller.rb
+++ app/controllers/calories_controller.rb
@@ -6,20 +6,20 @@ class CaloriesController < ApplicationController
   end
 
   def new
-    @calorie = Calorie.new
+    # @calorie = Calorie.new
   end
 
   def create
-    @user = current_user
-    @calorie = @user.calories.new(calorie_params)
-    if @calorie.save
-      redirect_to action: "index"
-      flash[:success] = "Your calories was saved successfully!"
-    else
-      # flash[:danger] = "Your calorie is more than 140 characters, please try making it shorter"
-      redirect_to new_calorie_path(@calorie)
-      flash[:danger] = "Your calories are invalid"
-    end
+    # @user = current_user
+    # @calorie = @user.calories.new(calorie_params)
+    # if @calorie.save
+    #   redirect_to action: "index"
+    #   flash[:success] = "Your calories was saved successfully!"
+    # else
+    #   # flash[:danger] = "Your calorie is more than 140 characters, please try making it shorter"
+    #   redirect_to new_calorie_path(@calorie)
+    #   flash[:danger] = "Your calories are invalid"
+    # end
   end
 
   def edit
